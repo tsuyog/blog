e00fa94e2ae7dd25c57e4f70e59667ae340ed330
tsuyog <cs.sarojbasnet@gmail.com>
2016-02-01 16:30:43 +1300

    Initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    #render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article Has bees saved"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:danger] ="Article has been deleted"
+    redirect_to articles_path
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(article_params)
+      flash[:success] = "Article Has bees saved"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+end
